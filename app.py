1524eb9b53a59d3440be67638dc2c117e5de9aec
jyotimishra2025 <itzjyotimishra@gmail.com>
2025-02-19 15:32:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -81,7 +81,7 @@ def user_input(user_question):
 
 def main():
     st.set_page_config("Chat PDF")
-    st.header("Chat with PDF using GenAI💁")
+    st.header("AI PDF Assistant 🤖📄")
 
     user_question = st.text_input("Ask a Question from the PDF Files")
 
